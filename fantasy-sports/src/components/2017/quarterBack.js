5e78536a323b0228683fc8b2df9921a6800871e7
robinchoii <robinchoii@gmail.com>
2018-06-14 17:39:50 -0700

    moving function to container

--- fantasy-sports/src/components/2017/quarterBack.js
+++ fantasy-sports/src/components/2017/quarterBack.js
@@ -14,13 +14,8 @@ export default class QuarterBack extends React.Component {
     }
 
     getPlayerStats = () => {
-        let config = {
-            auth: {
-                username: 'robinchoii',
-                password: 'fantasy123'
-              }
-        };
         let updatedIsSelected = Object.assign({}, this.state.isSelected);
+
         updatedIsSelected = true;
 
         this.setState({
@@ -31,35 +26,42 @@ export default class QuarterBack extends React.Component {
 
     componentDidMount = () => {
         console.log('player mounted')
-        var config = {
-            auth: {
-                username: 'robinchoii',
-                password: 'fantasy123'
-              },
-        };
 
-        axios.get(`https://api.mysportsfeeds.com/v1.2/pull/nfl/2017-regular/player_gamelogs.json?player=${this.props.firstname}-${this.props.lastname}-${this.props.playerID}`, config )
-            .then( (response) =>  {
-                let updatedWeeks = Object.assign( {}, this.state.weeks)
+        // this.getGameLogs()
 
-                updatedWeeks = response.data.playergamelogs['gamelogs']
-
-                this.setState({
-                    weeks: updatedWeeks
-                })
-
-            })
-            .catch( (err) => {
-                console.log(err)
-            })
 
 
     }
 
+    // getGameLogs = () => {
+    //     var config = {
+    //         auth: {
+    //             username: 'robinchoii',
+    //             password: 'fantasy123'
+    //           },
+    //     };
+
+    //     axios.get(`https://api.mysportsfeeds.com/v1.2/pull/nfl/2017-regular/player_gamelogs.json?player=${this.props.firstname}-${this.props.lastname}-${this.props.playerID}&&playerstats=Passing.td,Passing.att,Passing.yds,Passing.int,Rushing.td,Rushing.att,Rushing.yds,Fumbles.lost,2PT.2PTMade`, config )
+    //         .then( (response) =>  {
+    //             console.log(response.data.playergamelogs.gamelogs)
+    //             let updatedWeeks = Object.assign( {}, this.state.weeks)
+
+    //             updatedWeeks = response.data.playergamelogs.gamelogs
+
+    //             this.setState({
+    //                 weeks: updatedWeeks
+    //             })
+
+    //         })
+    //         .catch( (err) => {
+    //             console.log(err)
+    //         })
+    // }
+
     render() {
         return (
             <div>
-                <h5 onClick={this.getPlayerStats}> {this.props.firstname} {this.props.lastname} </h5>
+                <h5 onClick={this.props.getGameLogs(this.props.firstname,this.props.lastname, this.props.playerID)}> {this.props.firstname} {this.props.lastname} </h5>
                 { this.state.isSelected ?  (
                     <div>
                         <StatsContainer {...this.props} weeks={this.state.weeks} />
--- fantasy-sports/src/components/2017/quarterBackContainer.js
+++ fantasy-sports/src/components/2017/quarterBackContainer.js
@@ -20,23 +20,26 @@ export default class QuarterBackContainer extends React.Component {
     }
 
     componentDidMount() {
-        console.log('Quarterback container mounted!')
-        console.log(this.state)
+        console.log('Quarterback container mounted!');
+        this.getCumulativeStats();
+    }
 
-        var config = {
+    getCumulativeStats = () => {
+        const config = {
             auth: {
                 username: 'robinchoii',
                 password: 'fantasy123'
               },
         };
 
-        axios.get("https://api.mysportsfeeds.com/v1.2/pull/nfl/2017-regular/cumulative_player_stats.json?position=QB", config)
+        axios.get("https://api.mysportsfeeds.com/v1.2/pull/nfl/2017-regular/cumulative_player_stats.json?position=QB&&playerstats=Passing.td,Passing.att,Passing.yds,Passing.int,Rushing.td,Rushing.att,Rushing.yds,Fumbles.lost,2PT.2PTMade", config)
             .then((response) => {
-                console.log(response.data['cumulativeplayerstats'])
+                // console.log(response.data['cumulativeplayerstats']['playerstatsentry'])
                 let updatedQB = Object.assign({}, this.state.QB)
 
                 updatedQB = response.data['cumulativeplayerstats']['playerstatsentry']
 
+                // console.log(updatedQB)
                 this.setState({
                     QB: updatedQB
                 })
@@ -45,6 +48,22 @@ export default class QuarterBackContainer extends React.Component {
                 console.log(err)
             })
     }
+
+    onGetGameLogs = (first,last,ID) => {
+        console.log('click')
+        const config = {
+            auth: {
+                username: 'robinchoii',
+                password: 'fantasy123'
+              },
+        };
+        axios.get(`https://api.mysportsfeeds.com/v1.2/pull/nfl/2017-regular/player_gamelogs.json?player=${first}-${last}-${ID}&&playerstats=Passing.td,Passing.att,Passing.yds,Passing.int,Rushing.td,Rushing.yds,Fumbles.lost,2PT.2PTMade`, config)
+            .then((response) => {
+                console.log(response.data)
+            })
+
+    }
+
 
     getFantasyPoints = (pYds, pTD, int, rYds, rTD, fumL) => {
         let totalPoints = 0;
@@ -58,9 +77,6 @@ export default class QuarterBackContainer extends React.Component {
 
         return Math.ceil(totalPoints * 100) / 100
     }
-
-
-
 
     render() {
         return (
@@ -91,6 +107,8 @@ export default class QuarterBackContainer extends React.Component {
                         rushTD= { rushTD }
                         fumLost= { fumLost }
                         playerID= { player.player.ID}
+                        getGameLogs= { this.onGetGameLogs }
+
                         fantasyPoints = { this.getFantasyPoints(passYards, passTD, interceptions, rushYards, rushTD, fumLost)}/>
                 }
                 )}
