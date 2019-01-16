d429413faa82fe490b18eeb4f59dc5753ab91a25
Robin Choi <robinchoii@gmail.com>
2019-01-15 17:24:43 -0800

    add redux store and reducers, not finished

---
+++ fantasy-sports/src/reducers/quarterbacks.js
@@ -0,0 +1,8 @@
+export function( state=[], action) {
+    switch(action.type) {
+        case "FETCH_QUARTERBACKS":
+            return
+        default:
+            return state;
+    }
+}
---
+++ fantasy-sports/src/store.js
@@ -0,0 +1,12 @@
+import { createStore } from 'redux';
+const store = createStore(todos, ['Use Redux'])
+
+function addTodo(text) {
+  return {
+    type: 'ADD_TODO',
+    text
+  }
+}
+
+store.dispatch(addTodo('Read the docs'))
+store.dispatch(addTodo('Read about the middleware'))
