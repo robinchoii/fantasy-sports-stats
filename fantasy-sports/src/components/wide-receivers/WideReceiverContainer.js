6619504ff4640d091cc315193e308dfffcc549ed
Robin Choi <robinchoii@gmail.com>
2019-01-19 16:31:39 -0800

    successful axios call for 2018 player gamelogs

--- fantasy-sports/src/components/wide-receivers/WideReceiverContainer.js
+++ fantasy-sports/src/components/wide-receivers/WideReceiverContainer.js
@@ -8,7 +8,8 @@ export default class WideReceiverContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            WRs: []
+            WRs: [],
+            selectedWR: {}
         }
     }
 
@@ -38,6 +39,24 @@ export default class WideReceiverContainer extends React.Component {
     handleOnClick = (first, last, id) => {
         console.log('wr click');
         console.log(first, last, id)
+        let config = {
+            auth: {
+                username: 'e1c3c9b7-346a-4e73-a4ea-dc799d',
+                password: 'MYSPORTSFEEDS'
+              },
+        };
+
+        axios.get(`https://api.mysportsfeeds.com/v2.0/pull/nfl/2018-regular/player_gamelogs.json?player=${first}-${last}-${id}`, config)
+            .then((response) => {
+                // console.log(response.data)
+                let updatedSelectedWR = Object.assign({}, this.state.selectedWR);
+                updatedSelectedWR = response.data
+
+                console.log(updatedSelectedWR)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 
     render() {
