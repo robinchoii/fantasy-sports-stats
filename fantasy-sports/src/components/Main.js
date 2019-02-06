9b82c9ccf39ae2a639abfa398b33d2e19e11a283
Robin Choi <robinchoii@gmail.com>
2019-02-05 19:36:42 -0800

    update text

--- fantasy-sports/src/components/Main.js
+++ fantasy-sports/src/components/Main.js
@@ -4,7 +4,7 @@ export default class Main extends React.Component {
     render() {
         return (
             <div className='main main-container'>
-                <h2>Check out the best Fantasy Football Player's Stats from  Tables to Charts!</h2>
+                <h2>Visualize Fantasy Football Player's Stats from  Tables to Graphs!</h2>
                 <div>
                     <h4>Look for trends!</h4>
                     <i class="fas fa-4x fa-chart-line"></i>
