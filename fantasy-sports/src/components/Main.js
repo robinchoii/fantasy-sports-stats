efe4a6b5cade339d6b787cdd0196f7bfd686c32e
Robin Choi <robin_choi@apple.com>
2019-03-09 16:39:44 -0800

    fix layout on small vp

--- fantasy-sports/src/components/Main.js
+++ fantasy-sports/src/components/Main.js
@@ -6,10 +6,6 @@ export default class Main extends React.Component {
         return (
             <div className='main main-container'>
                 <h2>Visualize Fantasy Football Player's Stats from  Tables to Graphs!</h2>
-                <div>
-                    <h4>Look for trends!</h4>
-                    <i className="fas fa-4x fa-chart-line"></i>
-                </div>
                 <Settings />
             </div>
         );
